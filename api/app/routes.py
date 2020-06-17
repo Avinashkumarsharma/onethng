cd8a76906261c6a08d639995f865cd7ca6d7764a
avk <avi.loyola@gmail.com>
2020-06-17 18:23:32 +0530

    Update Settings to add cognito context header mapping

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,5 +1,16 @@
 from flask import current_app as app
-
-@app.route('/ping')
+from flask import request
+@app.route('/')
 def home():
-    return "pong"
+    return "Welcome Home"
+@app.route('/v1')
+def v1():
+    return "Welcome Home v1"
+@app.route('/dev')
+def dev():
+    return "Welcome Home dev"
+@app.route('/v1/dev')
+def dev2():
+    userid = request.headers.get('user_id')
+    return "Welcome Home v1 dev " + str(request.headers)
+
