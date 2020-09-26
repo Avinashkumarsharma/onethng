b34b12aa4a520c3e309f43dd59dfd56965aeabc7
avk <avi.loyola@gmail.com>
2020-09-26 11:57:19 +0530

    Update Prod env Users

---
+++ api/app/models/user.py
@@ -0,0 +1,3 @@
+class User(object):
+    def showname(self):
+        return "UserTest"
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,14 +1,16 @@
 from flask import current_app as app
 from flask import request
+from app.models import User
 @app.route('/')
 def home():
     return "Welcome Home"
 @app.route('/v1')
 def v1():
     return "Welcome Home v1"
-@app.route('/dev')
+@app.route('/test')
 def dev():
-    return "Welcome Home dev"
+    user = User();
+    return "Welcome Home dev" + user.showname();
 @app.route('/v1/dev')
 def dev2():
     userid = request.headers.get('user_id')
